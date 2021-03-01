61983687a1e85c4192d7e6aad5fa748fd99709b1
DiWu <wud2@union.edu>
2021-03-01 21:03:50 +0800

    implement hash demo 4

--- hash_fusion_demos/hash_demo3.py
+++ hash_fusion_demos/hash_demo3.py
@@ -33,7 +33,8 @@ def read_trajectory(filename):
 def main():
     cam_intr = np.loadtxt("../datasets/dataset_kitchen/camera-intrinsics.txt", delimiter=' ')
     camera_pose = read_trajectory("../datasets/dataset_stonewall/stonewall_trajectory.log")
-    n_imgs = len(camera_pose)
+    # n_imgs = len(camera_pose)
+    n_imgs = 1000
     vol_bnds = np.zeros((3, 2))
 
     for i in range(1, n_imgs + 1):
@@ -48,10 +49,10 @@ def main():
 
     hash_table = hash_fusion.HashTable(vol_bnds, voxel_size=0.02)
 
-    # Loop through RGB-D images and fuse them together
     t0_elapse = time.time()
+    total_time = 0
     for i in range(1, n_imgs + 1):
-        print("Fusing frame %d/%d" % (i, n_imgs))
+        tic = time.perf_counter()
 
         # Read RGB-D image and camera pose
         color_image = cv2.cvtColor(cv2.imread("../datasets/dataset_stonewall/stonewall_png/color/%06d.png" % i),
@@ -65,6 +66,12 @@ def main():
         # Integrate observation into voxel volume (assume color aligned with depth)
         hash_table.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
 
+        toc = time.perf_counter()
+        tictoc = round(toc - tic, 2)
+        total_time += tictoc
+        avg_time = round(total_time / i, 2)
+        print("Integrate frame {} in {} seconds. Avg: {}s/frame".format(i, tictoc, avg_time))
+
     fps = n_imgs / (time.time() - t0_elapse)
     print("Average FPS: {:.2f}".format(fps))
 
@@ -75,5 +82,54 @@ def main():
     grid_fusion.pcwrite("../meshes/pc_hash_demo3.ply", point_cloud)
 
 
+def fuse_odd_frames():
+    cam_intr = np.loadtxt("../datasets/dataset_kitchen/camera-intrinsics.txt", delimiter=' ')
+    camera_pose = read_trajectory("../datasets/dataset_stonewall/stonewall_trajectory.log")
+    n_imgs = len(camera_pose) // 2
+    vol_bnds = np.zeros((3, 2))
+
+    for i in range(n_imgs):
+        ith_frame = i * 2 + 1
+        depth_im = cv2.imread("../datasets/dataset_stonewall/stonewall_png/depth/%06d.png" % ith_frame, -1).astype(
+            np.float32)
+        depth_im /= 1000.
+        depth_im[depth_im == 65.535] = 0
+        cam_pose = camera_pose[i * 2]  # camera pose starts at index 0, which is paired with frame 1
+        view_frust_pts = grid_fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
+        vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], np.amin(view_frust_pts, axis=1))
+        vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], np.amax(view_frust_pts, axis=1))
+
+    hash_table = hash_fusion.HashTable(vol_bnds, voxel_size=0.02)
+
+    total_time = 0
+    for i in range(n_imgs):
+        tic = time.perf_counter()
+        ith_frame = i * 2 + 1
+        # Read RGB-D image and camera pose
+        color_image = cv2.cvtColor(cv2.imread("../datasets/dataset_stonewall/stonewall_png/color/%06d.png" % ith_frame),
+                                   cv2.COLOR_BGR2RGB)
+        depth_im = cv2.imread("../datasets/dataset_stonewall/stonewall_png/depth/%06d.png" % ith_frame, -1).astype(
+            np.float32)
+        depth_im /= 1000.
+        depth_im[depth_im == 65.535] = 0
+        cam_pose = camera_pose[i * 2]
+
+        # Integrate observation into voxel volume (assume color aligned with depth)
+        hash_table.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
+
+        toc = time.perf_counter()
+        tictoc = round(toc - tic, 2)
+        total_time += tictoc
+        avg_time = round(total_time / (i+1), 2)
+        print("Integrate frame {} in {} seconds. Avg: {}s/frame".format(ith_frame, tictoc, avg_time))
+
+    verts, faces, norms, colors = hash_table.get_mesh()
+    grid_fusion.meshwrite("../meshes/mesh_hash_demo3.ply", verts, faces, norms, colors)
+
+    point_cloud = hash_table.get_point_cloud()
+    grid_fusion.pcwrite("../meshes/pc_hash_demo3.ply", point_cloud)
+
+
 if __name__ == "__main__":
-    main()
+    fuse_odd_frames()
+    # main()
--- hash_fusion_demos/hash_demo4.py
+++ hash_fusion_demos/hash_demo4.py
@@ -13,19 +13,6 @@ from pycallgraph import PyCallGraph
 from pycallgraph.output import GraphvizOutput
 
 
-class CameraPose:
-    """
-    citation: http://redwood-data.org/indoor/fileformat.html
-    """
-    def __init__(self, meta, mat):
-        self.metadata = meta
-        self.pose = mat
-
-    def __str__(self):
-        return 'Metadata : ' + ' '.join(map(str, self.metadata)) + '\n' + \
-            "Pose : " + "\n" + np.array_str(self.pose)
-
-
 def read_trajectory(filename):
     """
     Citation: http://redwood-data.org/indoor/fileformat.html
@@ -34,105 +21,111 @@ def read_trajectory(filename):
     with open(filename, 'r') as f:
         metastr = f.readline()
         while metastr:
-            metadata = map(int, metastr.split())
-            mat = np.zeros(shape = (4, 4))
+            mat = np.zeros(shape=(4, 4))
             for i in range(4):
-                matstr = f.readline();
+                matstr = f.readline()
                 mat[i, :] = np.fromstring(matstr, dtype=float, sep=' \t')
-            traj.append(CameraPose(metadata, mat))
+            traj.append(mat)
             metastr = f.readline()
     return traj
 
 
-def ten_frame_test():
-    cam_intr = np.loadtxt("../data/camera-intrinsics.txt", delimiter=' ')
-    camera_pose = read_trajectory("../dataset_stonewall/stonewall_trajectory.log")
-    n_imgs = len(camera_pose)
-    vol_bnds = np.zeros((3, 2))
-
-    for i in range(n_imgs):
-        depth_im = cv2.imread("../data/frame-%06d.depth.png" % (i), -1).astype(float)
-        depth_im /= 1000.
-        depth_im[depth_im == 65.535] = 0
-        cam_pose = camera_pose[i]
-        view_frust_pts = grid_fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
-        vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], np.amin(view_frust_pts, axis=1))
-        vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], np.amax(view_frust_pts, axis=1))
-
-    hash_table = hash_fusion.HashTable(vol_bnds, voxel_size=0.02)
-
-    # Loop through RGB-D images and fuse them together
-    t0_elapse = time.time()
-    for i in range(10):
-        print("Fusing frame %d/%d" % (i + 1, n_imgs))
-
-        # Read RGB-D image and camera pose
-        color_image = cv2.cvtColor(cv2.imread("../data/frame-%06d.color.jpg" % (i)), cv2.COLOR_BGR2RGB)
-        depth_im = cv2.imread("../data/frame-%06d.depth.png" % (i), -1).astype(float)
-        depth_im /= 1000.
-        depth_im[depth_im == 65.535] = 0
-        cam_pose = np.loadtxt("../data/frame-%06d.pose.txt" % (i))
-
-        # Integrate observation into voxel volume (assume color aligned with depth)
-        hash_table.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
-
-    fps = n_imgs / (time.time() - t0_elapse)
-    print("Average FPS: {:.2f}".format(fps))
-
-    # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
-    print("Saving mesh to mesh.ply...")
-    verts, faces, norms, colors = hash_table.get_mesh()
-    grid_fusion.meshwrite("mesh_hash_demo4.ply", verts, faces, norms, colors)
-
-    # Get point cloud from voxel volume and save to disk (can be viewed with Meshlab)
-    point_cloud = hash_table.get_point_cloud()
-    grid_fusion.pcwrite("pc_hash_demo4.ply", point_cloud)
-
-
 def main():
-    cam_intr = np.loadtxt("../data/camera-intrinsics.txt", delimiter=' ')
-    camera_pose = read_trajectory("../dataset_stonewall/stonewall_trajectory.log")
-    n_imgs = len(camera_pose)
+    cam_intr = np.loadtxt("../datasets/dataset_kitchen/camera-intrinsics.txt", delimiter=' ')
+    camera_pose = read_trajectory("../datasets/dataset_lounge/lounge_trajectory.log")
+    # n_imgs = len(camera_pose)
+    n_imgs = 1000
     vol_bnds = np.zeros((3, 2))
 
-    for i in range(n_imgs):
-        depth_im = cv2.imread("../data/frame-%06d.depth.png" % (i), -1).astype(float)
+    for i in range(1, n_imgs + 1):
+        depth_im = cv2.imread("../datasets/dataset_lounge/lounge_png/depth/%06d.png" % i, -1).astype(
+            np.float32)
         depth_im /= 1000.
         depth_im[depth_im == 65.535] = 0
-        cam_pose = camera_pose[i]
+        cam_pose = camera_pose[i - 1]
         view_frust_pts = grid_fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
         vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], np.amin(view_frust_pts, axis=1))
         vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], np.amax(view_frust_pts, axis=1))
 
     hash_table = hash_fusion.HashTable(vol_bnds, voxel_size=0.02)
 
-    # Loop through RGB-D images and fuse them together
-    t0_elapse = time.time()
-    for i in range(n_imgs):
-        print("Fusing frame %d/%d" % (i + 1, n_imgs))
+    total_time = 0
+    for i in range(1, n_imgs + 1):
+        tic = time.perf_counter()
 
         # Read RGB-D image and camera pose
-        color_image = cv2.cvtColor(cv2.imread("../data/frame-%06d.color.jpg" % (i)), cv2.COLOR_BGR2RGB)
-        depth_im = cv2.imread("../data/frame-%06d.depth.png" % (i), -1).astype(float)
+        color_image = cv2.cvtColor(cv2.imread("../datasets/dataset_lounge/lounge_png/color/%06d.png" % i),
+                                   cv2.COLOR_BGR2RGB)
+        depth_im = cv2.imread("../datasets/dataset_lounge/lounge_png/depth/%06d.png" % i, -1).astype(
+            np.float32)
         depth_im /= 1000.
         depth_im[depth_im == 65.535] = 0
-        cam_pose = np.loadtxt("../data/frame-%06d.pose.txt" % (i))
+        cam_pose = camera_pose[i - 1]
 
         # Integrate observation into voxel volume (assume color aligned with depth)
         hash_table.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
 
-    fps = n_imgs / (time.time() - t0_elapse)
-    print("Average FPS: {:.2f}".format(fps))
+        toc = time.perf_counter()
+        tictoc = round(toc - tic, 2)
+        total_time += tictoc
+        avg_time = round(total_time / i, 2)
+        print("Integrate frame {} in {} seconds. Avg: {}s/frame".format(i, tictoc, avg_time))
 
-    # Get mesh from voxel volume and save to disk (can be viewed with Meshlab)
-    print("Saving mesh to mesh.ply...")
     verts, faces, norms, colors = hash_table.get_mesh()
-    grid_fusion.meshwrite("mesh_hash_demo4.ply", verts, faces, norms, colors)
+    grid_fusion.meshwrite("../meshes/mesh_hash_demo4.ply", verts, faces, norms, colors)
 
-    # Get point cloud from voxel volume and save to disk (can be viewed with Meshlab)
     point_cloud = hash_table.get_point_cloud()
-    grid_fusion.pcwrite("pc_hash_demo4.ply", point_cloud)
+    grid_fusion.pcwrite("../meshes/pc_hash_demo4.ply", point_cloud)
+
+
+def fuse_odd_frames():
+    cam_intr = np.loadtxt("../datasets/dataset_kitchen/camera-intrinsics.txt", delimiter=' ')
+    camera_pose = read_trajectory("../datasets/dataset_lounge/lounge_trajectory.log")
+    n_imgs = len(camera_pose) // 2
+    vol_bnds = np.zeros((3, 2))
+
+    for i in range(n_imgs):
+        ith_frame = i * 2 + 1
+        depth_im = cv2.imread("../datasets/dataset_lounge/lounge_png/depth/%06d.png" % ith_frame, -1).astype(
+            np.float32)
+        depth_im /= 1000.
+        depth_im[depth_im == 65.535] = 0
+        cam_pose = camera_pose[i * 2]  # camera pose starts at index 0, which is paired with frame 1
+        view_frust_pts = grid_fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
+        vol_bnds[:, 0] = np.minimum(vol_bnds[:, 0], np.amin(view_frust_pts, axis=1))
+        vol_bnds[:, 1] = np.maximum(vol_bnds[:, 1], np.amax(view_frust_pts, axis=1))
+
+    hash_table = hash_fusion.HashTable(vol_bnds, voxel_size=0.02)
+
+    total_time = 0
+    for i in range(n_imgs):
+        tic = time.perf_counter()
+        ith_frame = i * 2 + 1
+        # Read RGB-D image and camera pose
+        color_image = cv2.cvtColor(cv2.imread("../datasets/dataset_lounge/lounge_png/color/%06d.png" % ith_frame),
+                                   cv2.COLOR_BGR2RGB)
+        depth_im = cv2.imread("../datasets/dataset_lounge/lounge_png/depth/%06d.png" % ith_frame, -1).astype(
+            np.float32)
+        depth_im /= 1000.
+        depth_im[depth_im == 65.535] = 0
+        cam_pose = camera_pose[i * 2]
+
+        # Integrate observation into voxel volume (assume color aligned with depth)
+        hash_table.integrate(color_image, depth_im, cam_intr, cam_pose, obs_weight=1.)
+
+        toc = time.perf_counter()
+        tictoc = round(toc - tic, 2)
+        total_time += tictoc
+        avg_time = round(total_time / (i+1), 2)
+        print("Integrate frame {} in {} seconds. Avg: {}s/frame".format(ith_frame, tictoc, avg_time))
+
+    verts, faces, norms, colors = hash_table.get_mesh()
+    grid_fusion.meshwrite("../meshes/mesh_hash_demo4.ply", verts, faces, norms, colors)
+
+    point_cloud = hash_table.get_point_cloud()
+    grid_fusion.pcwrite("../meshes/pc_hash_demo4.ply", point_cloud)
 
 
 if __name__ == "__main__":
-    main()
+    fuse_odd_frames()
+    # main()
